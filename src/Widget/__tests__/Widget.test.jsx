f0e8f3b14677f1bc747b62f744a9ef5b8f4e5fb5
Pontus Alexander <pontus.alexander@gmail.com>
2017-12-16 20:41:34 +0100

    Add basic test suite.

---
+++ src/Widget/__tests__/Widget.test.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Widget from "../";
+
+it("renders logo face when given nothing", () => {
+  const tree = renderer.create(<Widget />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders throbber when given busy flag", () => {
+  const tree = renderer.create(<Widget busy />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders URL face when given url", () => {
+  const tree = renderer.create(<Widget url="https://my.text/url" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
